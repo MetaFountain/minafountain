23f7bca8edc6ede28bf4a18da7984e4c67aa0c2b
Nasfame <laciferin2024@protonmail.com>
2024-11-02 07:47:21 +0530

    solution:

--- packages/chain/test/runtime/modules/Sudoku.test.ts
+++ packages/chain/test/runtime/modules/Sudoku.test.ts
@@ -21,6 +21,7 @@ let number = 16
 
 const sudoku = generateSudoku(0.5)
 const solution = solveSudoku(sudoku)
+console.log("solution: " + solution)
 if (solution == undefined) throw Error("Devcon1: shouldnt happen")
 
 const signer = PrivateKey.random()
