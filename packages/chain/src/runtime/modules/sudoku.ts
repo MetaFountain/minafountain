0ae4fc3387a3202351e024d07679eae44f0cdfff
Nasfame <laciferin2024@protonmail.com>
2024-11-02 06:34:25 +0530

    almost: ready build

--- packages/chain/src/runtime/modules/sudoku.ts
+++ packages/chain/src/runtime/modules/sudoku.ts
@@ -22,7 +22,7 @@ export class ISudoku extends Struct({
 }
 
 @runtimeModule()
-export class Sudoku extends RuntimeModule<ISudoku> {
+export class Sudoku extends RuntimeModule<Record<any, never>> {
   @state() public sudokuHash = State.from<Field>(Field)
   @state() public isSolved = State.from<Bool>(Bool)
 
