2407ed2c86daec48d3338580211d0b000628ba93
Nasfame <laciferin2024@protonmail.com>
2024-11-17 12:07:13 +0700

    Home

--- apps/web/app/home/page.tsx
+++ apps/web/app/home/page.tsx
@@ -1,23 +1,23 @@
 'use client';
 
-// import { useState } from 'react';
+import { useState } from 'react';
 
-// export default function NewPage() {
-//   const [count, setCount] = useState(0);
+export default function Home() {
+  const [count, setCount] = useState(0);
 
-//   return (
-//     <div>
-//       <h1>Welcome to the new page!</h1>
-//       <p>You clicked {count} times</p>
-//     <button onClick={() => setCount(count + 1)}>
-//         Click me
-//       </button>
-//     </div>
-//   );
-// }
-
-export default function Home(){
   return (
-    <div> Home</div>
-  )
+    <div>
+      <h1>Welcome to the new page!</h1>
+      <p>You clicked {count} times</p>
+    <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
 }
+
+// export default function Home(){
+//   return (
+//     <div> Home</div>
+//   )
+// }
